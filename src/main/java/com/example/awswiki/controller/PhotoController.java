2d1319a33a1d26fafd1d81f7a51e32882308a601
Sewoong <lympsw12@naver.com>
2024-04-02 15:24:26 +0900

    api 매핑 추가

--- src/main/java/com/example/awswiki/controller/PhotoController.java
+++ src/main/java/com/example/awswiki/controller/PhotoController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class PhotoController {
     private final PhotoService photoService;
     @GetMapping("/photo")
